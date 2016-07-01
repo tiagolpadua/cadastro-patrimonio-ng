4020696688f40c59d0b4f7e3dca5b59ae71eb428
tiagolpadua <tiagolpadua@gmail.com>
2016-07-01 06:14:20 -0300

    slides

---
+++ client_gabarito/app/app.filters.js
@@ -0,0 +1,16 @@
+(function() {
+'use strict';
+
+angular
+    .module('cadpatFilters')
+    .filter('maiusculo', maiusculo);
+
+maiusculo.$inject = [];
+
+function maiusculo() {
+    return function(input) {
+        return input.toUpperCase();
+    };
+}
+})();
+
